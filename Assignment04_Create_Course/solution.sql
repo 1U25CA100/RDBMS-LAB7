8f5a598de944628ccf97c01cd964ee5add8583be
VISHWALINGAM <v73569889@gmail.com>
2026-08-27 11:29:33 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -2,8 +2,9 @@ DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
--- Create Course table
-
--- Insert three records
-
--- Display structure
+CREATE TABLE Course (
+    CourseID NUMBER(5),
+    CourseName VARCHAR2(30),
+    Credits NUMBER(2),
+    DepartmentID NUMBER(5)
+);
